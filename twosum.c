ebdc8f3622e0227413a60b8b13ac58922c9d8bb3
L9 <33311703+LUOJIUzxy@users.noreply.github.com>
2020-11-09 22:52:27 +0800

    Add files via upload

---
+++ twosum.c
@@ -0,0 +1,15 @@
+int* twoSum(int* nums, int numsSize, int target, int *returnSize){
+    int i,j;
+    *returnSize = NULL;
+    for( i = 0; i < numsSize-1; i++ ){
+        for( j = i+1 ; j < numsSize-1 ; j++ ){
+            if(nums[i] + nums[j] == target){
+                returnSize=(int*)malloc(sizeof(int)*2);
+                returnSize[0] = i;
+                returnSize[1] = j;
+                return returnSize;
+            }
+        }
+    }
+    return returnSize;
+}
