3ecbc9e5435c632154aec0a8dea8ddd8a690bed9
Xiyue Zhang <phoebez_99@outlook.com>
2025-02-22 23:24:57 +0100

    update
    update

--- Graph/disjointSet_quickUnion.py
+++ Graph/disjointSet_quickUnion.py
@@ -1,68 +1,74 @@
 # maintain an array
 # root_array = [0, 0, 0, 1, 4, 5, 5, 5, 4, 9]
-root_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#root_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+class QuickUnion:
+    # Add a constructor here for the root(parent)
+    def __init__(self, size) -> None:
+        self.root_array = [0] * size
+        for i in range(size):
+            self.root_array[i] = i
 
-# Add a constructor here for the root(parent)
+    # find the head of a node
+    def find(self, node_index):
+        node_value = self.root_array[node_index]
+        # when index matches the value, this is a head node
+        if node_index == node_value:
+            return node_index
+        else:
+            return self.find(self.root_array[node_value])
+        
+    def is_connected(self, node1_index, node2_index):
+        root_index1 = self.find(node1_index)
+        root_index2 = self.find(node2_index)
 
-# find the head of a node
-def find(node_index):
-    node_value = root_array[node_index]
-    # when index matches the value, this is a head node
-    if node_index == node_value:
-        return node_index
-    else:
-        return find(root_array[node_value])
-    
-def is_connected(node1_index, node2_index):
-    root_index1 = find(node1_index)
-    root_index2 = find(node2_index)
+        if root_index1 == root_index2:
+            return True
+        else:
+            return False
 
-    if root_index1 == root_index2:
-        return True
-    else:
-        return False
+    # input is two-element set
+    def union(self, edge_tuple):
+        if edge_tuple[0] < edge_tuple[1]:
+            vertice1 = edge_tuple[0]
+            vertice2 = edge_tuple[1]
+        else:
+            vertice1 = edge_tuple[1]
+            vertice2 = edge_tuple[0]
 
-# input is two-element set
-def union(edge_tuple):
-    if edge_tuple[0] < edge_tuple[1]:
-        vertice1 = edge_tuple[0]
-        vertice2 = edge_tuple[1]
-    else:
-        vertice1 = edge_tuple[1]
-        vertice2 = edge_tuple[0]
+        # if root_array[vertice1] == vertice1:
+        #     root_array[vertice1] = vertice1
+        # print(vertice1)
+        self.root_array[vertice2] = vertice1
+        print(self.root_array[vertice2])
 
-    # if root_array[vertice1] == vertice1:
-    #     root_array[vertice1] = vertice1
-    # print(vertice1)
-    root_array[vertice2] = vertice1
-    print(root_array[vertice2])
-
-def connect(node1_index, node2_index):
-    if node1_index < node2_index:
-        # set node2 as the parent node of node1
-        parent_of_node1 = find(node1_index)
-        # and set the parent node of node 1 's parent to node2, so all the children nodes of it would have node2 as parent as well
-        root_array[parent_of_node1] = node2_index
+    def connect(self, node1_index, node2_index):
+        if node1_index < node2_index:
+            # set node2 as the parent node of node1
+            parent_of_node1 = self.find(node1_index)
+            # and set the parent node of node 1 's parent to node2, so all the children nodes of it would have node2 as parent as well
+            self.root_array[parent_of_node1] = node2_index
     
 
 if __name__ == "__main__":
     number = 10
     edges = {(0, 1), (0, 2), (1, 3), (4, 8), (5, 6), (5, 7)}
 
+    quickUnion = QuickUnion(10)
+    
 
-    print(root_array)
+    print(quickUnion.root_array)
     for tuple in edges:
-        union(tuple)
-    print(root_array)
+        quickUnion.union(tuple)
+    print(quickUnion.root_array)
 
-    #print(find(3))
+    print(quickUnion.find(3))
 
     vertices_tbchecked = {(0, 3), (1, 5), (7, 8)}
     for vertices in vertices_tbchecked:
-        print(is_connected(vertices[0], vertices[1]))
+        print(quickUnion.is_connected(vertices[0], vertices[1]))
 
-    connect(7, 8)
+    quickUnion.connect(7, 8)
     vertices_tbchecked = {(7, 8), (4, 6), (8, 5)}
     for vertices in vertices_tbchecked:
-        print(is_connected(vertices[0], vertices[1]))
+        print(quickUnion.is_connected(vertices[0], vertices[1]))
