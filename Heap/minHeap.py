9193d1df134a8c565dac6cb6a9ecac5fc798c259
Xiyue Zhang <phoebez_99@outlook.com>
2025-03-26 17:02:11 +0100

    add heapify

--- Heap/minHeap.py
+++ Heap/minHeap.py
@@ -1,15 +1,47 @@
 # Implementing "Min Heap"
+import math
+
 class MinHeap:
-    def __init__(self, heapSize):
+    
+    def __init__(self, heapSize, array):
         # Create a complete binary tree using an array
         # Then use the binary tree to construct a Heap
-        self.heapSize = heapSize
+        if heapSize + 1 != len(array):
+            print("Array size does not match!")
+            return
+        self.heapSize = heapSize 
         # the number of elements is needed when instantiating an array
         # heapSize records the size of the array
-        self.minheap = [0] * (heapSize + 1)
+        #self.minheap = [0] * (heapSize + 1)
+        self.minheap = array
         # realSize records the number of elements in the Heap
         self.realSize = 0
+    
+    # level of this complete binary tree
+    def length(self):
+        return self.heapSize.bit_length()
 
+    def heapify(self, node_index):
+        smallest = node_index
+        left = smallest * 2
+        right = smallest * 2 + 1
+
+        if left > self.heapSize or right > self.heapSize:
+            return
+
+        if self.minheap[smallest] > self.minheap[left] or self.minheap[smallest] > self.minheap[right] :
+            if self.minheap[left] >= self.minheap[right] :
+                self.minheap[smallest], self.minheap[right] = self.minheap[right], self.minheap[smallest]
+                
+                self.heapify(right)
+            else:
+                self.minheap[smallest], self.minheap[left] = self.minheap[left], self.minheap[smallest]
+                
+                self.heapify(left)
+            
+        
+
+    # O(logN)
     # Function to add an element
     def add(self, element):
         self.realSize += 1
@@ -40,14 +72,16 @@ class MinHeap:
             parent = index // 2
     
     # Get the top element of the Heap
+    # O(1)
     def peek(self):
         return self.minheap[1]
     
+
     # Delete the top element of the Heap
     def pop(self):
         # If the number of elements in the current Heap is 0,
         # print "Don't have any elements" and return a default value
-        if self.realSize < 1:
+        if self.heapSize < 1:
             print("Don't have any element!")
             return 
             #return sys.maxsize
@@ -56,11 +90,11 @@ class MinHeap:
             # self.realSize >= 1
             removeElement = self.minheap[1]
             # Put the last element in the Heap to the top of Heap
-            self.minheap[1] = self.minheap[self.realSize]
-            self.realSize -= 1
+            self.minheap[1] = self.minheap[self.heapSize]
+            self.heapSize -= 1
             index = 1
             # When the deleted element is not a leaf node
-            while (index <= self.realSize // 2):
+            while (index <= self.heapSize // 2):
                 # the left child of the deleted element
                 left = index * 2
                 # the right child of the deleted element
@@ -79,22 +113,39 @@ class MinHeap:
                     break
             return removeElement
     
+    # o(1)
     # return the number of elements in the Heap
     def size(self):
-        return self.realSize
+        return self.heapSize
     
     def __str__(self):
-        return str(self.minheap[1 : self.realSize + 1])
+        return str(self.minheap[1 : self.heapSize + 1])
         
 
 if __name__ == "__main__":
     	# Test cases
-        minHeap = MinHeap(5)
-        minHeap.add(3)
-        minHeap.add(1)
-        minHeap.add(2)
+        num_nodes = 5
+        arr = [3, 1, 2, 6, 5]
+
+        # add a 0 in the front
+        arr.insert(0, 0)
+
+        minHeap = MinHeap(num_nodes, arr)
+        # minHeap.add(3)
+        # minHeap.add(1)
+        # minHeap.add(2)
+     
+        level = minHeap.length()
+        print(level)
+        print(minHeap.minheap)
+
+        for j in range(1, level):
+            for i in range(pow(2, (level - 1 - j)), pow(2, (level - j))):
+                print(i)
+                minHeap.heapify(i)
+
         # [1,3,2]
-        print(minHeap)
+        print(minHeap.minheap)
         # 1
         print(minHeap.peek())
         # 1
@@ -106,4 +157,4 @@ if __name__ == "__main__":
         minHeap.add(4)
         minHeap.add(5)
         # [4,5]
-        print(minHeap)
+        print(minHeap.minheap)
